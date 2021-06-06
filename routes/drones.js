822c882223e58fd99f58cb326f8434fee088a4dd
Linda Villa <linda.villa@alea.com>
2021-06-06 22:09:56 +0200

    seed DB

--- routes/drones.js
+++ routes/drones.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const Drone = require('../models/drone.model.js');
 
 // require the Drone model here
 
 const router = express.Router();
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+
+
+router.get('/drones', (req, res) => {
+ Drone.find()
+ .then(showAllDrones => {
+   res.render('drones/list', { drones: showAllDrones });
+ })
+ .catch(error => {console.log('error showing all drones', error)
+})
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+
 });
 
 router.post('/drones/create', (req, res, next) => {
